9c9b771f0fc56501a22f4663064340f918188f87
nathan lopez <cherrygal2011@hotmail.com>
2015-02-04 15:00:18 -0800

    blog 15 video doing well

---
+++ controller/create-db.php
@@ -0,0 +1,36 @@
+<?php
+     require_once(__DIR__ . "/../model/database.php");
+
+     $connection = new mysqli($host, $username, $password);
+     // my connection for mysqli
+     
+     if($connection->connect_error) {
+         die("Error: ".$connection->connect_error );
+     }  
+   
+     
+     $exists = $connection->select_db($database);
+     
+     if(!$exists) {
+         $query = $connection->query("CREATE DATABASE $database");
+         
+         
+      if($query) {
+          echo "sucessful create database" . $database;
+      }
+     }
+     else {
+     echo "Database already exists.";
+         
+     }
+     
+     $query = $connection->query("CREATE TABLE  posts ("
+             ."id int(11) NOT NULL AUTO_INCREMENT,"
+             ."title varchar(255)NOT NULL,"
+             ."post textNOT NULL,"
+             ."PRIMARY KEY (id))");
+     
+     if($query){
+      echo "Succesfully create table:posts";
+     }
+     $connection->close();
