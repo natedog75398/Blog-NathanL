caae08fd5390fc3e3c92390fb04b9671a4fbcf09
nathan lopez <cherrygal2011@hotmail.com>
2015-02-25 15:01:05 -0800

    editing the project blog

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
+<html>
+    <head>
 <link type='text/css' rel='stylesheet' href='css/bootstrap-theme.css'>
 <link type='text/css' rel='stylesheet' href='css/bootstrap.css'>
 <link type="text/css" rel="stylesheet" href="controller/index.css">
 
+
 <link rel='shortcut icon' href='http://www.fatescustomz.com/wp-content/uploads/2014/08/MTGz-Sticker-copy.jpg'>
+    </head>
 
 
-
+<body>
 <div class="container">
 <div class="jumbotron">
-<div class="dropdown">
+    <h1>Welcome to MTGz Blog</h1>
     
-    <h1> Welcome to the MTGz Blog</h1>
-  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-    Dropdown
-    <span class="caret"></span>
+    <!-- Single button -->
+<div class="btn-group">
+  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+    MTGz<span class="caret"></span>
   </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="login.php">Login to mtgz blog</a></li>
-    <li role="presentation"><a role="menuitem" tabindex="-1" href="register.php">register for mtgz blog</a></li>
-    <li role="presentation"><a role="menuitem" tabindex="-1" href="controller/logout-user.php">Logout of the mtgz blog</a></li>
-    
+  <ul class="dropdown-menu" role="menu">
+    <li><a  href="login.php">Login to mtgz blog</a></li>
+    <li><a  href="register.php">register for mtgz blog</a></li>
+    <li><a  href="controller/logout-user.php">Logout of the mtgz blog</a></li>
   </ul>
+</div>
 
-</div>
-<div class="dropdown">
-  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-    Portfolio 3
-    <span class="caret"></span>
+<!-- Single button -->
+<div class="btn-group">
+  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+    Portfolio 3 <span class="caret"></span>
   </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="portfolio-3-Nathan-Lopez-master/public_html/index.html">About Me</a></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="portfolio-3-Nathan-Lopez-master/public_html/project.html">Projects</a></li>
-      <li role="presentation"><a role="menuitem" tabindex="-1" href="portfolio-3-Nathan-Lopez-master/public_html/tech.html">Technology</a></li>
-   
+  <ul class="dropdown-menu" role="menu">
+      <li><a href="portfolio-3-Nathan-Lopez-master/public_html/index.html">About me</a></li>
+      <li><a href="portfolio-3-Nathan-Lopez-master/public_html/project.html">Projects</a></li>
+      <li><a href="portfolio-3-Nathan-Lopez-master/public_html/tech.html">Tech</a></li>
   </ul>
 </div>
 </div>
 </div>
+
 
 <?php
     require_once (__DIR__ . "/controller/login-verify.php");
--- view/header.php
+++ view/header.php
@@ -6,6 +6,9 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <link type='text/css' rel='stylesheet' href='css/bootstrap-theme.css'>
+    <link type='text/css' rel='stylesheet' href='css/bootstrap.css'>
+    <link type="text/css" rel="stylesheet" href="controller/index.css">
         <meta charset="UTF-8">
         <title></title>
     </head>
